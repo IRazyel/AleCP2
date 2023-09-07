463579713e12480306c7314c8051d251b8eef270
Razyel420 <razyel.arboleya@gmail.com>
2023-09-06 21:00:49 -0300

    app.jsx arrumado e main.jsx criado

--- src/main.jsx
+++ src/main.jsx
@@ -7,3 +7,30 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
+import EditarProdutos from './routes/EditarProdutos/EditarProdutos.jsx';
+import Home from './routes/Home/Home.jsx';
+import Produtos from './routes/Produtos/Produtos.jsx';
+import Error from './routes/Error/Error.jsx';
+
+const router = createBrowserRouter([
+  { path: '/' , element: <App/>,
+    errorElement: <Error/>,
+    children: [
+      { path: '/' , element: <Home/>},
+      { path: '/produtos' , element: <Produtos/>},
+      { path: '/produtos/editar/:id' , element: <EditarProdutos/>},
+      { path: '/antiga' , element: <Navigate to='/'/> }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
