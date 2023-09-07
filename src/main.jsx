f4d690dcec278f061ffba7b8f583ba56d8595f62
Razyel420 <razyel.arboleya@gmail.com>
2023-09-06 21:05:47 -0300

    pagina main.jsx arrumda

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-
 import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
 import EditarProdutos from './routes/EditarProdutos/EditarProdutos.jsx';
 import Home from './routes/Home/Home.jsx';
